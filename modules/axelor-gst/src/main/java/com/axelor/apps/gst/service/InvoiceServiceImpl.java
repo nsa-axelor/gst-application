87a8b3c5faff1598513a06af20f0b2e2e9b60675
nsa.axelor <nsa.axelor@gmail.com>
2020-01-31 11:41:27 +0530

    Invoice line correction for saved invoices

--- modules/axelor-gst/src/main/java/com/axelor/apps/gst/service/InvoiceServiceImpl.java
+++ modules/axelor-gst/src/main/java/com/axelor/apps/gst/service/InvoiceServiceImpl.java
@@ -13,53 +13,51 @@ import java.util.List;
 
 public class InvoiceServiceImpl implements InvoiceService {
 
-  @Inject InvoiceLineService invoiceLineService;
+	@Inject
+	InvoiceLineService invoiceLineService;
 
-  @Override
-  public Invoice getCalculatedInvoice(Invoice invoice, List<InvoiceLine> lineList) {
-    BigDecimal totalNetAmount = new BigDecimal(0);
-    BigDecimal netIgst = new BigDecimal(0);
-    BigDecimal netCgst = new BigDecimal(0);
-    BigDecimal netSgst = new BigDecimal(0);
-    BigDecimal netGrossAmount = new BigDecimal(0);
-    for (InvoiceLine line : lineList) {
-      BigDecimal netAmount = line.getNetAmount();
-      totalNetAmount = totalNetAmount.add(netAmount);
-      netIgst = netIgst.add(line.getIgst());
-      netSgst = netSgst.add(line.getSgst());
-      netCgst = netCgst.add(line.getCsgt());
-      netGrossAmount = netGrossAmount.add(line.getGrossAmount());
-    }
+	@Override
+	public Invoice getCalculatedInvoice(Invoice invoice, List<InvoiceLine> lineList) {
+		BigDecimal totalNetAmount = new BigDecimal(0);
+		BigDecimal netIgst = new BigDecimal(0);
+		BigDecimal netCgst = new BigDecimal(0);
+		BigDecimal netSgst = new BigDecimal(0);
+		BigDecimal netGrossAmount = new BigDecimal(0);
+		for (InvoiceLine line : lineList) {
+			BigDecimal netAmount = line.getNetAmount();
+			totalNetAmount = totalNetAmount.add(netAmount);
+			netIgst = netIgst.add(line.getIgst());
+			netSgst = netSgst.add(line.getSgst());
+			netCgst = netCgst.add(line.getCsgt());
+			netGrossAmount = netGrossAmount.add(line.getGrossAmount());
+		}
 
-    invoice.setNetAmount(totalNetAmount);
-    invoice.setNetCsgt(netCgst);
-    invoice.setNetIgst(netIgst);
-    invoice.setNetSgst(netSgst);
-    invoice.setGrossAmount(netGrossAmount);
-    return invoice;
-  }
+		invoice.setNetAmount(totalNetAmount);
+		invoice.setNetCsgt(netCgst);
+		invoice.setNetIgst(netIgst);
+		invoice.setNetSgst(netSgst);
+		invoice.setGrossAmount(netGrossAmount);
+		return invoice;
+	}
 
-  @Override
-  public Address getAddressForShipping(Invoice invoice, Party party, Company company) {
-    if (!invoice.getIsUseInvoiceAddressAsShipping()) {
-      return Query.of(Address.class)
-          .filter("(self.type = 'shipping' OR self.type = 'default') AND self.party = :party")
-          .bind("party", party)
-          .fetchOne();
-    }
+	@Override
+	public Address getAddressForShipping(Invoice invoice, Party party, Company company) {
+		if (!invoice.getIsUseInvoiceAddressAsShipping()) {
+			return Query.of(Address.class)
+					.filter("(self.type = 'shipping' OR self.type = 'default') AND self.party = :party")
+					.bind("party", party).fetchOne();
+		}
 
-    return Query.of(Address.class)
-        .filter("(self.type = 'invoice' OR self.type = 'default') AND self.party = :party")
-        .bind("party", party)
-        .fetchOne();
-  }
+		return Query.of(Address.class)
+				.filter("(self.type = 'invoice' OR self.type = 'default') AND self.party = :party").bind("party", party)
+				.fetchOne();
+	}
 
-  @Override
-  public List<InvoiceLine> regenerateInvoiceLine(Invoice invoice) {
-    List<InvoiceLine> lines = invoice.getInvoiceItemList();
-    List<InvoiceLine> invoiceItemList = new ArrayList<>();
-    lines.forEach(
-        line -> invoiceItemList.add(invoiceLineService.calculateInvoiceLine(line, invoice)));
-    return invoiceItemList;
-  }
+	@Override
+	public List<InvoiceLine> regenerateInvoiceLine(Invoice invoice) {
+		List<InvoiceLine> lines = invoice.getInvoiceItemList();
+		List<InvoiceLine> invoiceItemList = new ArrayList<>();
+		lines.forEach(line -> invoiceItemList.add(invoiceLineService.calculateInvoiceLine(line, invoice)));
+		return invoiceItemList;
+	}
 }
--- modules/axelor-gst/src/main/java/com/axelor/apps/gst/web/InvoiceController.java
+++ modules/axelor-gst/src/main/java/com/axelor/apps/gst/web/InvoiceController.java
@@ -21,80 +21,85 @@ import java.util.List;
 
 public class InvoiceController {
 
-  @Inject SequenceService sequenceService;
-  @Inject InvoiceService invoiceService;
+	@Inject
+	SequenceService sequenceService;
+	@Inject
+	InvoiceService invoiceService;
 
-  @Transactional
-  public void generateNewSequence(ActionRequest request, ActionResponse response) {
-    try {
-      MetaModelRepository metaModelRepository = Beans.get(MetaModelRepository.class);
-      MetaModel model = metaModelRepository.findByName("Invoice");
-      Sequence sequence = sequenceService.findSequenceByModel(model);
-      response.setValue("reference", sequenceService.generateSequence(sequence));
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+	@Transactional
+	public void generateNewSequence(ActionRequest request, ActionResponse response) {
+		try {
+			MetaModelRepository metaModelRepository = Beans.get(MetaModelRepository.class);
+			MetaModel model = metaModelRepository.findByName("Invoice");
+			Sequence sequence = sequenceService.findSequenceByModel(model);
+			response.setValue("reference", sequenceService.generateSequence(sequence));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-  public void setPartyPrimaryContactAndCalculations(
-      ActionRequest request, ActionResponse response) {
-    try {
-      Invoice invoice = request.getContext().asType(Invoice.class);
-      try {
-        Party party = invoice.getParty();
-        Contact contact =
-            Query.of(Contact.class)
-                .filter("self.type = 'primary' AND self.party = :party")
-                .bind("party", party)
-                .fetchOne();
-        Address address =
-            Query.of(Address.class)
-                .filter("(self.type = 'invoice' OR self.type = 'default') AND self.party = :party")
-                .bind("party", party)
-                .fetchOne();
-        Company company = invoice.getCompany();
+	public void setPartyPrimaryContactAndCalculations(ActionRequest request, ActionResponse response) {
+		try {
+			Invoice invoice = request.getContext().asType(Invoice.class);
+			try {
+				Party party = invoice.getParty();
+				Contact contact = Query.of(Contact.class).filter("self.type = 'primary' AND self.party = :party")
+						.bind("party", party).fetchOne();
+				Address address = Query.of(Address.class)
+						.filter("(self.type = 'invoice' OR self.type = 'default') AND self.party = :party")
+						.bind("party", party).fetchOne();
+				Company company = invoice.getCompany();
 
-        Address shippingAddress = invoiceService.getAddressForShipping(invoice, party, company);
-        invoice.setPartyContact(contact);
-        invoice.setInvoiceAddress(address);
-        invoice.setShippingAddress(shippingAddress);
-        List<InvoiceLine> lineList = invoice.getInvoiceItemList();
-        invoice.setInvoiceItemList(invoiceService.regenerateInvoiceLine(invoice));
-        invoice = invoiceService.getCalculatedInvoice(invoice, lineList);
-      } catch (NullPointerException e) {
-        e.printStackTrace();
-      }
-      response.setValues(invoice);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+				Address shippingAddress = invoiceService.getAddressForShipping(invoice, party, company);
+				invoice.setPartyContact(contact);
+				invoice.setInvoiceAddress(address);
+				invoice.setShippingAddress(shippingAddress);
+				List<InvoiceLine> lineList = invoice.getInvoiceItemList();
+				invoice.setInvoiceItemList(invoiceService.regenerateInvoiceLine(invoice));
+				invoice = invoiceService.getCalculatedInvoice(invoice, lineList);
+			} catch (NullPointerException e) {
+				e.printStackTrace();
+			}
+			response.setValue("invoiceItemList", invoice.getInvoiceItemList());
+//      response.setValues(invoice);
+			response.setValue("partyContact", invoice.getPartyContact());
+			response.setValue("invoiceAddress", invoice.getInvoiceAddress());
+			response.setValue("shippingAddress", invoice.getShippingAddress());
+			response.setValue("netAmount", invoice.getNetAmount());
+			response.setValue("netIgst", invoice.getNetIgst());
+			response.setValue("netCsgt", invoice.getNetCsgt());
+			response.setValue("netSgst", invoice.getNetSgst());
+			response.setValue("grossAmount", invoice.getGrossAmount());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-  // public void changeShippingAddress(ActionRequest request, ActionResponse
-  // response) {
-  // Invoice invoice = request.getContext().asType(Invoice.class);
-  // Party party = invoice.getParty();
-  // Company company = invoice.getCompany();
-  // Address address = invoiceService.getAddressForShipping(invoice,
-  // party,company);
-  // invoice.setShippingAddress(address);
-  // List<InvoiceLine> lineList = invoice.getInvoiceItemList();
-  // invoice.setInvoiceItemList(invoiceService.regenerateInvoiceLine(invoice));
-  // lineList.forEach(line -> System.out.println(line.getProduct().getName()));
-  //
-  // invoice = invoiceService.getCalculatedInvoice(invoice, lineList);
-  // response.setValues(invoice);
-  // }
+	// public void changeShippingAddress(ActionRequest request, ActionResponse
+	// response) {
+	// Invoice invoice = request.getContext().asType(Invoice.class);
+	// Party party = invoice.getParty();
+	// Company company = invoice.getCompany();
+	// Address address = invoiceService.getAddressForShipping(invoice,
+	// party,company);
+	// invoice.setShippingAddress(address);
+	// List<InvoiceLine> lineList = invoice.getInvoiceItemList();
+	// invoice.setInvoiceItemList(invoiceService.regenerateInvoiceLine(invoice));
+	// lineList.forEach(line -> System.out.println(line.getProduct().getName()));
+	//
+	// invoice = invoiceService.getCalculatedInvoice(invoice, lineList);
+	// response.setValues(invoice);
+	// }
 
-  public void calculateTotalTaxAndAmounts(ActionRequest request, ActionResponse response) {
-    try {
-      Invoice invoice = request.getContext().asType(Invoice.class);
-      List<InvoiceLine> lineList = invoice.getInvoiceItemList();
-      invoice.setInvoiceItemList(invoiceService.regenerateInvoiceLine(invoice));
-      invoice = invoiceService.getCalculatedInvoice(invoice, lineList);
-      response.setValues(invoice);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+	public void calculateTotalTaxAndAmounts(ActionRequest request, ActionResponse response) {
+		try {
+			Invoice invoice = request.getContext().asType(Invoice.class);
+			List<InvoiceLine> lineList = invoice.getInvoiceItemList();
+			invoice.setInvoiceItemList(invoiceService.regenerateInvoiceLine(invoice));
+			invoice = invoiceService.getCalculatedInvoice(invoice, lineList);
+			response.setValues(invoice);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
